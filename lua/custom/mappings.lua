8520f39bb6c661ecc5ba9eb46ad671211becfe18
David Gregorio Fuentes-Mendez <dgreegmdz@hotmail.com>
2023-09-03 09:25:24 -0600

    about to do 'nala install xclip' to see if then I can copy text from nvim to the system clipboard (to be able to paste it outside of nvim)

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -1,28 +1,57 @@
 local M = {}
 local opt = vim.opt
+-- This opt enables visual mode whenever pressing Shift:
+opt.keymodel = "startsel,stopsel"
 
---To disable default keymaps
+-- To disable keymaps
 M.disabled = {
-	c = {
-		--["<C-s>"] = "",
-		--["<leader>e"] = ""
+	n = {
+		-- ["<C-n>"] = "",
+		["<leader>/"] = "",
+	},
+	v = {
+		-- ["<C-s>"] = "",
+		["<leader>/"] = "",
 	}
 }
 
-opt.keymodel = "startsel,stopsel"
-
 -- My custom mappings
 M.davidfm = {
-	-- n = {},
+	n = {
+		["<leader>e"] = {"<cmd> NvimtreeToggle <Enter>", "Toggle Tree"},
+	},
 	i = {
 		["<C-s>"] = {"<cmd> w <Enter>", "Save File"}
 	},
 	-- v = {},
 	c = {
-		["<C-s>"] = {"<C-\\>e \"lua require('telescope.builtin').command_history "
-    .. '{ default_text = [=[" . escape(getcmdline(), \'"\') . "]=] }"<CR><CR>', "Fuzzy Command Search$"},
-		["<C-k>"] = {"<cmd> Telescope keymaps <Enter>"}
+		["<C-c>"] = {"<C-\\>e \"lua require('telescope.builtin').command_history "
+		.. '{ default_text = [=[" . escape(getcmdline(), \'"\') . "]=] }"<Enter><Enter>', "Fuzzy Command Search"},
+		["<C-k>"] = {"<cmd> Telescope keymaps <Enter>", "Fuzzy Keymap Search"},
+		["<C-v>"] = {"<cmd> Telescope lsp_document_symbols <Enter>", "Fuzzy Variable Search"}
 	}
+}
+
+-- My custom way of commenting
+M.comment = {
+  plugin = true,
+
+  -- toggle comment in both modes
+  n = {
+    ["<leader>co"] = {
+      function()
+        require("Comment.api").toggle.linewise.current()
+      end,
+      "Toggle comment",
+    },
+  },
+
+  v = {
+    ["<leader>co"] = {
+      "<ESC><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<CR>",
+      "Toggle comment",
+    },
+  },
 }
 
 return M
